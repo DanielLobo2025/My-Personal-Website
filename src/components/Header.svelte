5ca5e20826fe8bfc89736e5e4dd87740d2ee122b
Daniel Lobo <132111858+DanielLobo2025@users.noreply.github.com>
2023-10-25 12:46:53 -0400

    Add files via upload

---
+++ src/components/Header.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let y;
+
+    let tabs = [
+        {name:"Projects", link:"#projects"},
+        {name:"About Me", link:"#about"},
+        {name:"Resume", link:"https://drive.google.com/file/d/1n30AWhds1pZrlEPD6xKhINjXZjQoyPV3/view?usp=sharing"},
+
+];
+        
+
+
+</script>
+ <header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid ' + 
+ (  y > 0 
+        ? "py-4 bg-slate-950 border-violet-950"
+        : "py-6 bg-transparent border-transparent"
+ )}>
+    <h1 class="font-medium">
+        <b class = "font-bold poppins">Daniel Lobo</b> 
+    </h1>
+    <div class="sm:flex items-center gap-4 hidden">
+        {#each tabs as tab, index}
+        <a href={tab.link} class="duration-200 hover:text-violet-400">
+            <p>{tab.name}</p>
+        </a>
+        <div/>
+        {/each}
+
+    <a href = "https://www.linkedin.com/in/csdaniellobo/"
+    target = "_blank"
+    class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white
+    text-slate-950">
+    <div class = "absolute top-0 right-full w-full h-full bg-violet-400 opacity-20
+    group-hover:translate-x-full z-0 duration-200">
+</div>
+<h4 class = "relative z-9">Get in touch</h4>
+</a>
+    
+
+ </header>
